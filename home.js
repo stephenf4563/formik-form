2ad8744473bd2eea92c311583ecc6863b48a7df6
stephenf4563 <82065722+stephenf4563@users.noreply.github.com>
2021-10-13 20:45:40 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,12 @@
+function Home(){
+  return (
+    <Card
+      bgcolor="success"
+      txtcolor="black"
+      header="BadBank Landing Module"
+      title="Welcome to the bank"
+      text="You can move around using the navigation bar."
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
