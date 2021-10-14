ff9e9b77528b11bd52eb7d983a32a91a537caabf
stephenf4563 <82065722+stephenf4563@users.noreply.github.com>
2021-10-13 21:22:09 -0400

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -1,5 +1,15 @@
 function Deposit(){
   return (
+    <Card
+      bgcolor="success"
+      txtcolor="black"
+      header="BadBank Landing Module"
+      title="Welcome to the bank"
+      text="You can move around using the navigation bar."
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
     <h1>Deposit</h1>
   )
 }
