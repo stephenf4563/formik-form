b8dd7e6dc02b76a2d0998f4dfca97f5d41722e57
stephenf4563 <82065722+stephenf4563@users.noreply.github.com>
2021-10-17 11:11:33 -0400

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -1,14 +1,9 @@
 function AllData(){
   const ctx = React.useContext(UserContext);
- return (
-   <Card
-      bgcolor="success"
-      txtcolor="black"
-      title="all Entered Data"
-      body={<h5>All Data in Store</h5>
-    {JSON.stringify(ctx)}
-<br/>)
-}
-    />    
-  );  
+  return (
+    <>
+    <h5>All Data in Store</h5>
+    {JSON.stringify(ctx)}<br/>
+    </>
+  );
 }
