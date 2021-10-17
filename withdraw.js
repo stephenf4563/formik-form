468a1e647359334aec20b36ba2cb5abbc880b880
Ryan McCarthy <Ryanmc852@gmail.com>
2021-10-17 09:36:21 -0400

    Update withdraw.js
    Fixed typo on withdraw function name.

--- withdraw.js
+++ withdraw.js
@@ -1,7 +1,7 @@
 function load(){
   ctx.users.name;
 }
-function Withdrawl(){
+function Withdraw(){
 const [show, setShow]         = React.useState(true);
 const [status, setStatus]     = React.useState('');
 const [name, setName]         = React.useState('');
