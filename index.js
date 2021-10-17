541572a2c67d2c471f1f238e5ddcb38dc30ccf18
stephenf4563 <82065722+stephenf4563@users.noreply.github.com>
2021-10-17 08:59:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function Spa() {
         <div className="container" style={{padding: "20px"}}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
-          <Route path="/login/" component={Login} />
           <Route path="/deposit/" component={Deposit} />
           <Route path="/withdraw/" component={Withdraw} />
           <Route path="/balance/" component={Balance} />
