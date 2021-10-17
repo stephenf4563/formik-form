76cb306ca4bc5fb146151ce86a9f0da0c26cb2d9
stephenf4563 <82065722+stephenf4563@users.noreply.github.com>
2021-10-17 09:21:09 -0400

    Update withdraw.js

--- withdraw.js
+++ withdraw.js
@@ -1,7 +1,7 @@
 function load(){
   ctx.users.name;
 }
-function Deposit(){
+function Withdrawl(){
 const [show, setShow]         = React.useState(true);
 const [status, setStatus]     = React.useState('');
 const [name, setName]         = React.useState('');
