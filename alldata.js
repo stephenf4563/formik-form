40eae18122581e81482036a3355f8a3c3a9b495b
Ryan McCarthy <Ryanmc852@gmail.com>
2021-10-17 11:24:51 -0400

    Improving allData display.

--- alldata.js
+++ alldata.js
@@ -1,5 +1,20 @@
 function AllData(){
   const ctx = React.useContext(UserContext);
+
+  function beautifyData() {
+    var prettyString="";
+
+    var users = ctx.users;
+    for(var i = 0; i < users.length; i++) {
+        prettyString+=users[i].name +"\n"
+        prettyString+=users[i].email +"\n"
+        prettyString+=users[i].password +"\n"
+        prettyString+=users[i].balance +"\n"
+    }
+
+    return prettyString;
+  }
+
   return (
     <Card
       bgcolor="primary"
@@ -7,7 +22,7 @@ function AllData(){
       status={status}
       body={ <>
         <h5>All Data in Store</h5>
-        {ctx.users[0].name +"\n" + ctx.users[0].email}<br/>
+        {beautifyData()}<br/>
         </>
       }
   />
