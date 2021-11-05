d0ff08596b641256ce7a58a2a0ca068b8562ce1e
Ryan McCarthy <Ryanmc852@gmail.com>
2021-11-05 13:24:27 -0400

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -65,7 +65,6 @@ function Deposit(){
     selected = nameVal;
     for(var i = 0; i < ctx.users.length; i++) {
       if(selected === ctx.users[i].name){
-        ctx.users[i].balance = ctx.users[i].balance+tempVal;
         setStatus("balance " + ctx.users[i].balance);
       }
     }
