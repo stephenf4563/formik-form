1cc22d8065b9063f80a11a8fd55cc565dec357d9
Ryan McCarthy <Ryanmc852@gmail.com>
2021-11-05 13:21:27 -0400

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -7,7 +7,7 @@ function Deposit(){
 
   
   const ctx = React.useContext(UserContext);
-  var selected = "";
+  var selected = ctx.users[0].name;
   var tempVal=0;
   function validate(field, label){
       if (!field) {
